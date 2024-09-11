796ae1d9a087203c6ff0d821c9741d755602f726
Pramoj PC <Pramoj PC@DESKTOP-8JGKH7G>
2024-09-11 12:18:55 -0700

    Added method to click on burger menu!

--- src/test/java/com/saucedemo/test/LogoutPageTests.java
+++ src/test/java/com/saucedemo/test/LogoutPageTests.java
@@ -1,7 +1,21 @@
 package com.saucedemo.test;
 
+import org.testng.annotations.Test;
+
+import com.saucedemo.pages.LogoutPage;
+
 public class LogoutPageTests extends BaseTest {
 	
-	
+
+	@Test
+	public void testLogout() throws InterruptedException {
+		login();
+		Logger = report.createTest("Click on the Burger Menu");
+		logoutpage.click_on_left_Menu();
+		Logger = report.createTest("Clicked on the Burger Menu");
+		Thread.sleep(4000);
+		
+		
+	}
 
 }
