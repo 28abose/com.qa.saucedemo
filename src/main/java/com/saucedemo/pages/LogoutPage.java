bce90828ae81353029434f565b0df65c6b0e0795
Pramoj PC <Pramoj PC@DESKTOP-8JGKH7G>
2024-09-11 12:46:50 -0700

    Some updates Testcases  in LogoutPage/TestPage

--- src/main/java/com/saucedemo/pages/LogoutPage.java
+++ src/main/java/com/saucedemo/pages/LogoutPage.java
@@ -1,5 +1,6 @@
 package com.saucedemo.pages;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.saucedemo.base.DriverScript;
@@ -9,15 +10,31 @@ import com.saucedemo.base.DriverScript;
 public class LogoutPage extends DriverScript {
 	
 	//locators
+	@FindBy(xpath="//button[@id='react-burger-menu-btn' and contains(text(),'Open Me')]") 
+	private WebElement leftMenu;
 	
+	@FindBy(xpath="//nav[@class='bm-item-list']")
+	private WebElement leftMenu_lists;
+	@FindBy(xpath="//nav[@class='bm-item-list']//a[@id='logout_sidebar_link']")
+	private WebElement logoutLink;
 	
 	//initialization
-
 	public LogoutPage() {
 		PageFactory.initElements(driver,this);
 	}
+	
 	//methods
 	
+	public void click_on_left_Menu() {
+		leftMenu.click();
+	}
 	
-
+	public void check_left_Menu_display() {
+		leftMenu_lists.isDisplayed();
+		
+	}
+	
+	public void click_on_logout() {
+		logoutLink.click();
+	}
 }
--- src/test/java/com/saucedemo/test/LogoutPageTests.java
+++ src/test/java/com/saucedemo/test/LogoutPageTests.java
@@ -1,5 +1,7 @@
 package com.saucedemo.test;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 import com.saucedemo.pages.LogoutPage;
@@ -8,12 +10,19 @@ public class LogoutPageTests extends BaseTest {
 	
 
 	@Test
-	public void testLogout() throws InterruptedException {
+	public void testLogoutFunctionality() throws InterruptedException {
 		login();
 		Logger = report.createTest("Click on the Burger Menu");
 		logoutpage.click_on_left_Menu();
 		Logger = report.createTest("Clicked on the Burger Menu");
 		Thread.sleep(4000);
+		Logger = report.createTest("check left meny lists are displayed");
+		logoutpage.check_left_Menu_display();
+		Logger = report.createTest("left menu lists are  all displayed");
+		//assert.assertEquals(null, null)
+		
+		logoutpage.click_on_logout();
+		
 		
 		
 	}
