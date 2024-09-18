3446f8f61291d1d28f89cf8ecfba5872cebea616
QA10000 <nsh.klr1@gmail.com>
2024-09-18 10:30:08 -0500

    Added logger statements and more tests

--- src/main/java/com/saucedemo/pages/CartPage.java
+++ src/main/java/com/saucedemo/pages/CartPage.java
@@ -4,97 +4,132 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import com.saucedemo.base.DriverScript;
+import java.util.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 public class CartPage extends DriverScript {
-	@FindBy(id="add-to-cart-sauce-labs-bike-light") 
+	@FindBy(id = "add-to-cart-sauce-labs-bike-light")
 	private WebElement cartButton;
-	@FindBy(xpath = "//div[@class=\"inventory_item_name\"]") 
+	@FindBy(xpath = "//div[@class=\"inventory_item_name\"]")
 	private WebElement cartItem;
 	@FindBy(xpath = "//a[@class='shopping_cart_link' and @data-test='shopping-cart-link']")
-    private WebElement shoppingCartLink;
-	@FindBy(id="remove-sauce-labs-bike-light") 
+	private WebElement shoppingCartLink;
+	@FindBy(id = "remove-sauce-labs-bike-light")
 	private WebElement removeItemButton;
-	@FindBy(id="checkout")
+	@FindBy(id = "checkout")
 	private WebElement checkoutButton;
-	@FindBy(id="first-name")
+	@FindBy(id = "first-name")
 	private WebElement firstNameField;
-	@FindBy(id="last-name")
+	@FindBy(id = "last-name")
 	private WebElement lastNameField;
-	@FindBy(id="postal-code")
+	@FindBy(id = "postal-code")
 	private WebElement zipCodeField;
-	@FindBy(id="continue")
+	@FindBy(id = "continue")
 	private WebElement checkoutConinueButton;
-	@FindBy(id="finish")
+	@FindBy(id = "finish")
 	private WebElement checkoutFinishButton;
-	@FindBy(className="summary_info")
+	@FindBy(className = "summary_info")
 	private WebElement summary;
-	@FindBy(className="summary_subtotal_label")
+	@FindBy(className = "summary_subtotal_label")
 	private WebElement SummarySubtotal;
+	@FindBy(id = "remove-sauce-labs-bike-light")
+	private WebElement removeButton;
+	@FindBy(xpath = "//div[@class=\"inventory_item_name\"]")
+    private WebElement itemOnCart;
+	@FindBy(id = id="continue-shopping)
+	private WebElement continuetoHomePageButton;
 	
-	
-	
+
 	public CartPage() {
-		PageFactory.initElements(driver,this);
+		PageFactory.initElements(driver, this);
 	}
-	
+
 	public void clickCartButton() {
 		cartButton.click();
-		
+
 	}
-	
+
 	public void validateItemText(String textActual, String textExpected) {
 		Assert.assertEquals(textActual, textExpected);
 	}
-	
+
 	public String getTextElement() {
 		return getText(cartItem);
 	}
-	
- public void clickCartLink() {
-	 shoppingCartLink.click();
- }
- 
- public void clickRemoveButton() {
-	 removeItemButton.click();
-	 
- }
- 
- public void clickCheckoutButton() {
-	 checkoutButton.click();
- }
- 
- public void enterFirstName(String firstname) {
-	 firstNameField.sendKeys(firstname);
-	 
- }
- 
- public void enterLastName(String lastname) {
-	 lastNameField.sendKeys(lastname);
- }
- 
- public void enterZipcode(String zipcode) {
-	 zipCodeField.sendKeys(zipcode);
-  }
- 
- public void clickContinueCheckoutButton() {
-	 checkoutConinueButton.click();
-	 
- }
- 
- public void clickFinishChekoutButton() {
-	 checkoutFinishButton.click();
- }
- 
- public void veryfyPaymentSummary() {
-	 Assert.assertTrue(summary.isDisplayed(), "Summary is not displayed");
- }
- 
- public void fillForm() {
-	enterFirstName("John");
-	enterLastName("Doe");
-	enterZipcode("60630");
-		
+
+	public void clickCartLink() {
+		shoppingCartLink.click();
 	}
+
+	public void clickRemoveButton() {
+		removeItemButton.click();
+
+	}
+
+	public void clickCheckoutButton() {
+		checkoutButton.click();
+	}
+
+	public void enterFirstName(String firstname) {
+		firstNameField.sendKeys(firstname);
+
+	}
+
+	public void enterLastName(String lastname) {
+		lastNameField.sendKeys(lastname);
+	}
+
+	public void enterZipcode(String zipcode) {
+		zipCodeField.sendKeys(zipcode);
+	}
+
+	public void clickContinueCheckoutButton() {
+		checkoutConinueButton.click();
+
+	}
+
+	public void clickFinishChekoutButton() {
+		checkoutFinishButton.click();
+	}
+
+	public void veryfyPaymentSummary() {
+		Assert.assertTrue(summary.isDisplayed(), "Summary is not displayed");
+	}
+
+	public void fillForm() {
+		enterFirstName("John");
+		enterLastName("Doe");
+		enterZipcode("60630");
+	}
+
+	public void clickRemoveitemCart() {
+		removeButton.click();
+	}
+	
+
+    public boolean isItemDisplayed() {
+        try {
+            WebElement item = itemOnCart;
+            return item.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public void verifyItemRemovedFromCart() {
+        assertFalse("Item should be removed from the cart", isItemDisplayed());
+    }
+
+
+private void assertFalse(String string, boolean itemDisplayed) {
+	// TODO Auto-generated method stub
+	
 }
 
+public void clickBContinueShoppingButtonfrmCart() {
+	continuetoHomePageButton.click();
+	
+}
+    
+
+}
--- src/test/java/com/saucedemo/test/CartPageTest.java
+++ src/test/java/com/saucedemo/test/CartPageTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class CartPageTest extends BaseTest {
 
-	//@Test
+	// @Test
 
 	public void testCartItems() throws InterruptedException {
 		Logger = report.createTest("Test Add Item to shopping cart");
@@ -20,52 +20,105 @@ public class CartPageTest extends BaseTest {
 		String actualText = cartpage.getTextElement();
 		cartpage.validateItemText(actualText, expectedText);
 		Logger.pass("Validated added item to cart successfully");
-}
-	@Test
+	}
+
+	// Add and remove item button tested from Home page
+	// @Test
 	public void testAddAndRemoveitem() throws InterruptedException {
+		Logger = report.createTest("Test remove item button on homepage");
 		login();
+		Logger.pass("User login successfully");
 		cartpage.clickCartButton();
+		Logger.pass(" User adds successfully from homepage");
 		Thread.sleep(3000);
 		cartpage.clickRemoveButton();
+		Logger.pass("Item removed successfully from homepage");
 		Thread.sleep(3000);
-}
-	//@Test
-	public void testCheckout() {
-		login();
-		cartpage.clickCartButton();
-		cartpage.clickCartLink();
-		cartpage.clickCheckoutButton();
+		// Have to add assert here?
 	}
-	//@Test
+
+	 @Test
+	public void testCheckout() {
+		Logger = report.createTest("Test checkout button clicking from shopping cart");
+		login();
+		Logger.pass("User login successfully");
+		cartpage.clickCartButton();
+		Logger.pass("User adds Item  successfully from homepage");
+		cartpage.clickCartLink();
+		Logger.pass("Shopping cart icon successfully clicked from homepage");
+		cartpage.clickCheckoutButton();
+		Logger.pass(" User successfully clicked checkout button from shopping cart");
+
+	}
+
+	 @Test
 	public void testCheckoutForm() throws InterruptedException {
-		testCheckout();
+		Logger = report.createTest("Test completion of checkout process");
+     	testCheckout();
+		Thread.sleep(3000);
+		Logger.pass("User proceed from shopping cart to next page successfully from shopping cart");
 		cartpage.fillForm();
+		Logger.pass("User fill user information successfully");
 		Thread.sleep(3000);
 		cartpage.clickContinueCheckoutButton();
+		Logger.pass("User click continue button to last page successfully");
 		cartpage.clickFinishChekoutButton();
+		Logger.pass("User click finish button to last page successfully");
 		Thread.sleep(3000);
-	    String pageTitle = homepage.getHomePageTitle();
+		String pageTitle = homepage.getHomePageTitle();
 		System.out.println(pageTitle);
 		String actualTitle = "Swag Labs";
 		if (pageTitle.equals(actualTitle)) {
-		System.out.println("TestPassed");
-		}
-		else 
-		{
+			System.out.println("TestPassed");
+		} else {
 			System.out.println("Testfailed");
 		}
-		
-		
+		Logger.pass("checkout process completed successfully");
+
 	}
 
-		@Test
+	 @Test
 	public void testCheckoutDetails() throws InterruptedException {
-			testCheckout();
+		Logger = report.createTest("Test payment details on checkout finish page");
+		testCheckout();
+		Logger.pass("User clicked checkout button successfully from shopping cart");
 		cartpage.fillForm();
-			Thread.sleep(3000);
-			cartpage.clickContinueCheckoutButton();
-			//testCheckoutForm();
+		Logger.pass("User fill user information successfully");
+		Thread.sleep(3000);
+		cartpage.clickContinueCheckoutButton();
+		Logger.pass("User click continue button to last page successfully");
+		// testCheckoutForm();
 		cartpage.veryfyPaymentSummary();
+		Logger.pass("Payment information verified successfully");
+	}
+	// @Test
+
+	public void testRemoveitemCartpage() throws InterruptedException {
+		Logger = report.createTest("Test removal of item from shopping cart");
+		login();
+		Logger.pass("User login successfully");
+		cartpage.clickCartButton();
+		Logger.pass("User added item successfully from homepage");
+		cartpage.clickCartLink();
+		Logger.info("User clicked the link to cart button successfully");
+		Thread.sleep(4000);
+		cartpage.clickRemoveitemCart();
+		Logger.pass("User rmoves item successfully from shopping cart");
+		cartpage.verifyItemRemovedFromCart();
+		Logger.pass("Item removal is verified successfully");
+
+	}
+	
+	// Test for clicking continue button back to home page
+	public void testContinuButtonFromCart() {
+		Logger = report.createTest("Test checkout button clicking from shopping cart");
+		login();
+		Logger.pass("User login successfully");
+		cartpage.clickCartButton();
+		Logger.pass("User adds Item  successfully from homepage");
+		cartpage.clickCartLink();
+		cartpage.clickBContinueShoppingButtonfrmCart();
+		
 	}
 
 }
